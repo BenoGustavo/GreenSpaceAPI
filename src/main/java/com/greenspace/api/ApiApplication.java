e7494895617feee67e38098fa3b2fbba3393be23
Gustavo <gustavoleandrogorges@gmail.com>
2024-10-24 20:03:11 -0300

    Add to APIApplication routes to index and api-docs endpoints

--- src/main/java/com/greenspace/api/ApiApplication.java
+++ src/main/java/com/greenspace/api/ApiApplication.java
@@ -2,9 +2,25 @@ package com.greenspace.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ApiApplication {
+
+	@GetMapping("/")
+	public String index() {
+		return "<strong>Olá mundo!</strong></br>Seja bem vindo, de uma olhada na documentação clicando aqui: <a href=\"/api-docs\" >api-docs</a>";
+	}
+
+	@GetMapping("/api-docs")
+	public RedirectView apiDocs() {
+		return new RedirectView("/swagger-ui.html");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
