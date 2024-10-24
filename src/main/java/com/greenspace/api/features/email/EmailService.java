82b3dcd1cdb6ad45f22841b3b4b0711b18fb2824
Gustavo <gustavoleandrogorges@gmail.com>
2024-10-24 20:10:38 -0300

    Add EmailService class for sending emails

---
+++ src/main/java/com/greenspace/api/features/email/EmailService.java
@@ -0,0 +1,44 @@
+package com.greenspace.api.features.email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import com.greenspace.api.dto.email.EmailDTO;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.from}")
+    private String from;
+
+    public void sendEmail(EmailDTO emailContent) {
+        SimpleMailMessage email = new SimpleMailMessage();
+        email.setFrom(from);
+        email.setTo(emailContent.getUserEmail());
+        email.setSubject(emailContent.getSubject());
+        email.setText(emailContent.getMessage());
+        mailSender.send(email);
+    }
+
+    // public void sendVerificationEmail(UserModel user, String token) {
+
+    // String recipientAddress = user.getEmailAddress();
+    // String subject = "Account Verification";
+    // String confirmationUrl = host + "/api/auth/verify-email?token=" + token;
+    // String message = "Clique no link para verificar sua conta:\n" +
+    // confirmationUrl;
+
+    // SimpleMailMessage email = new SimpleMailMessage();
+    // email.setFrom(from);
+    // email.setTo(recipientAddress);
+    // email.setSubject(subject);
+    // email.setText(message);
+    // mailSender.send(email);
+    // }
+}
