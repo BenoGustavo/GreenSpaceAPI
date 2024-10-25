0c6438b71b797b3e29a442432da6913b68078b53
Gustavo <gustavoleandrogorges@gmail.com>
2024-10-25 09:37:39 -0300

    Refactor UserRepository to add method for deleting all user tokens

--- src/main/java/com/greenspace/api/features/user/UserRepository.java
+++ src/main/java/com/greenspace/api/features/user/UserRepository.java
@@ -4,9 +4,20 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.greenspace.api.models.UserModel;
 
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
     Optional<UserModel> findByEmailAddress(String emailAddress);
+
+    Optional<UserModel> findByUsername(String username);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM TokenModel v WHERE v.user = :user")
+    void deleteAllUserTokens(@Param("user") UserModel user);
 }
