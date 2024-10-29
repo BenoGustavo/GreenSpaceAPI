98bd47db1f995d01db7d17d27d287685b937f8fc
Gustavo <gustavoleandrogorges@gmail.com>
2024-10-29 10:18:46 -0300

    Add UserProfileOauth2GmailDTO for OAuth2 Gmail user profile

---
+++ src/main/java/com/greenspace/api/dto/auth/UserProfileOauth2GmailDTO.java
@@ -0,0 +1,18 @@
+package com.greenspace.api.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class UserProfileOauth2GmailDTO {
+    private String id;
+    private String email;
+    private String verified_email;
+    private String name;
+    private String given_name;
+    private String family_name;
+    private String picture;
+}
