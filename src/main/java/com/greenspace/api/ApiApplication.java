de824197d6f3c319e1dcdc3f2637c0e0cb99bcaa
Gustavo <gustavoleandrogorges@gmail.com>
2024-11-20 19:57:06 -0300

    feat: enable scheduling in ApiApplication and refactor ApplicationConfiguration to use CustomUserDetailsService

--- src/main/java/com/greenspace/api/ApiApplication.java
+++ src/main/java/com/greenspace/api/ApiApplication.java
@@ -2,6 +2,7 @@ package com.greenspace.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.view.RedirectView;
 @SpringBootApplication
 @RestController
 @RequestMapping("/")
+@EnableScheduling
 public class ApiApplication {
 
 	@GetMapping("/")
--- src/main/java/com/greenspace/api/config/ApplicationConfiguration.java
+++ src/main/java/com/greenspace/api/config/ApplicationConfiguration.java
@@ -1,7 +1,5 @@
 package com.greenspace.api.config;
 
-import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,43 +7,19 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import com.greenspace.api.features.user.UserRepository;
-import com.greenspace.api.models.UserModel;
 
 @Configuration
 public class ApplicationConfiguration {
 
     @Autowired
-    private UserRepository userRepository;
-
-    @Bean
-    UserDetailsService userDetailsService() {
-        return email -> {
-            UserModel userEntity = userRepository.findByEmailAddress(email)
-                    .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
-
-            GrantedAuthority authority = new SimpleGrantedAuthority(userEntity.getPermissionLevel().toString());
-            var authorities = Set.of(authority);
-
-            return new User(
-                    userEntity.getEmailAddress(),
-                    userEntity.getPassword(),
-                    authorities);
-        };
-    }
+    private CustomUserDetailsService userDetailsService;
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
 
-        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
 
         return authProvider;
