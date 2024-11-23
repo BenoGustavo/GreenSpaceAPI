b44d21aa044980ea3ed1a1e30737671139e98f74
Gustavo <gustavoleandrogorges@gmail.com>
2024-11-23 15:44:02 -0300

    feat: add Validation utility class for Brazilian cellphone number and username validation

---
+++ src/main/java/com/greenspace/api/utils/Validation.java
@@ -0,0 +1,25 @@
+package com.greenspace.api.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+@Configuration
+public class Validation {
+    private final String BRAZILIAM_CELLPHONE_NUMBER_REGEX = "^\\+55\\d{2}9\\d{8}$";
+    public final Pattern BRAZILIAM_CELLPHONE_NUMBER_PATTERN = Pattern.compile(BRAZILIAM_CELLPHONE_NUMBER_REGEX);
+
+    private final String USERNAME_REGEX = "@([A-Za-z0-9._]{1,30})";
+    public final Pattern USERNAME_PATTERN = Pattern.compile(USERNAME_REGEX);
+
+    public boolean isFieldValid(String field, Pattern pattern) {
+        if (field == null) {
+            return false;
+        }
+        Matcher matcher = pattern.matcher(field);
+        return matcher.matches();
+    }
+}
