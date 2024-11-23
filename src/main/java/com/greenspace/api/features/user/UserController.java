109a7e042f99a62caa91d2ac6de04d393654bb18
Gustavo <gustavoleandrogorges@gmail.com>
2024-11-23 15:44:16 -0300

    feat: add updateLoggedUser endpoint and implement user update logic in UserService

--- src/main/java/com/greenspace/api/features/user/UserController.java
+++ src/main/java/com/greenspace/api/features/user/UserController.java
@@ -2,9 +2,11 @@ package com.greenspace.api.features.user;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.greenspace.api.dto.profile.UserUpdateDTO;
 import com.greenspace.api.dto.responses.Response;
 import com.greenspace.api.models.UserModel;
 
@@ -30,4 +32,16 @@ public class UserController {
         return ResponseEntity.ok(response);
     }
 
+    @PatchMapping("/update-logged-user")
+    public ResponseEntity<Response<Object>> updateLoggedUser(UserUpdateDTO updatedUserUpdateDTO) {
+        UserModel updatedUser = userService.updateLoggedUser(updatedUserUpdateDTO);
+
+        Response<Object> response = Response.builder()
+                .message("User updated successfully")
+                .status(200)
+                .data(updatedUser)
+                .build();
+
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/com/greenspace/api/features/user/UserService.java
+++ src/main/java/com/greenspace/api/features/user/UserService.java
@@ -2,19 +2,23 @@ package com.greenspace.api.features.user;
 
 import org.springframework.stereotype.Service;
 
+import com.greenspace.api.dto.profile.UserUpdateDTO;
 import com.greenspace.api.error.http.NotFound404Exception;
 import com.greenspace.api.jwt.Jwt;
 import com.greenspace.api.models.UserModel;
+import com.greenspace.api.utils.Validation;
 
 @Service
 public class UserService {
 
     private final UserRepository repository;
     private final Jwt jwtManager;
+    private final Validation validationUtil;
 
-    public UserService(UserRepository repository, Jwt jwtManager) {
+    public UserService(UserRepository repository, Jwt jwtManager, Validation validationUtil) {
         this.repository = repository;
         this.jwtManager = jwtManager;
+        this.validationUtil = validationUtil;
     }
 
     public UserModel getLoggedUser() {
@@ -24,5 +28,31 @@ public class UserService {
                 .orElseThrow(() -> new NotFound404Exception("Perhaps the user is not logged in."));
 
         return loggedUser;
+    }
+
+    // TEM QUE ARRUMAR POR QUE ISSO DAQUI TEM QUE LANÇAR UM ERRO QUANDO NÃO PASSA
+    // #TODO
+    public UserModel updateLoggedUser(UserUpdateDTO updatedUserUpdateDTO) {
+        UserModel loggedUser = getLoggedUser();
+
+        if (validationUtil.isFieldValid(
+                updatedUserUpdateDTO.getUsername(),
+                validationUtil.USERNAME_PATTERN)) {
+
+            loggedUser.setUsername(updatedUserUpdateDTO.getUsername());
+        }
+
+        if (validationUtil.isFieldValid(
+                updatedUserUpdateDTO.getPhoneNumber(),
+                validationUtil.BRAZILIAM_CELLPHONE_NUMBER_PATTERN)) {
+
+            loggedUser.setPhoneNumber(updatedUserUpdateDTO.getPhoneNumber());
+        }
+
+        if (updatedUserUpdateDTO.getNickname() != null) {
+            loggedUser.setNickname(updatedUserUpdateDTO.getNickname());
+        }
+
+        return repository.save(loggedUser);
     }
 }
