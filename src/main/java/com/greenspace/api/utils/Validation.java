20e275bf3d5a437c2ca82a38286c58fdd9157583
Gustavo <gustavoleandrogorges@gmail.com>
2024-11-26 14:27:48 -0300

    feat: update Brazilian cellphone number regex for improved validation

--- src/main/java/com/greenspace/api/utils/Validation.java
+++ src/main/java/com/greenspace/api/utils/Validation.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Component;
 @Component
 @Configuration
 public class Validation {
-    private final String BRAZILIAM_CELLPHONE_NUMBER_REGEX = "^\\+55\\d{2}9\\d{8}$";
+    // +55 (11) 98888-8888 / 9999-9999 / 21 98888-8888 / 5511988888888
+    // ESSE REGEX NÃO TA FUNCIONANDO
+    private final String BRAZILIAM_CELLPHONE_NUMBER_REGEX = "/^(?:(?:\\+|00)?(55)\\s?)?(?:\\(?([1-9][0-9])\\)?\\s?)?(?:((?:9\\d|[2-9])\\d{3})\\-?(\\d{4}))$/";
     public final Pattern BRAZILIAM_CELLPHONE_NUMBER_PATTERN = Pattern.compile(BRAZILIAM_CELLPHONE_NUMBER_REGEX);
 
     private final String USERNAME_REGEX = "@([A-Za-z0-9._]{1,30})";
