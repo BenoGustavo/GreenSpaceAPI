6ad37b888ed67ddfe36bf8b58f32c8a227654fc0
Gustavo <gustavoleandrogorges@gmail.com>
2024-11-29 21:52:57 -0300

    feat: add methods to UserImagesRepository for filtering user images by type and fetching DTOs

--- src/main/java/com/greenspace/api/features/imagesManager/UserImagesRepository.java
+++ src/main/java/com/greenspace/api/features/imagesManager/UserImagesRepository.java
@@ -9,17 +9,26 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.greenspace.api.dto.UserImagesDTO;
 import com.greenspace.api.enums.ImageType;
 import com.greenspace.api.models.UserImagesModel;
 
 public interface UserImagesRepository extends JpaRepository<UserImagesModel, UUID> {
-    List<UserImagesModel> findByUserId(UUID userId);
+        List<UserImagesModel> findByUserId(UUID userId);
 
-    @Query("SELECT COUNT(u) FROM UserImagesModel u WHERE u.user.id = :userId")
-    long countUserImageQuantity(@Param("userId") UUID userId);
+        @Query("SELECT COUNT(u) FROM UserImagesModel u WHERE u.user.id = :userId")
+        long countUserImageQuantity(@Param("userId") UUID userId);
 
-    @Modifying
-    @Transactional
-    @Query("DELETE FROM UserImagesModel u WHERE u.user.id = :userId AND u.imageType = :imageType")
-    void deleteUserImageByType(@Param("userId") UUID userId, @Param("imageType") ImageType imageType);
+        @Modifying
+        @Transactional
+        @Query("DELETE FROM UserImagesModel u WHERE u.user.id = :userId AND u.imageType = :imageType")
+        void deleteUserImageByType(@Param("userId") UUID userId, @Param("imageType") ImageType imageType);
+
+        List<UserImagesModel> findByUserIdAndImageType(UUID userId, ImageType imageType);
+
+        @Query("SELECT new com.greenspace.api.dto.UserImagesDTO(u.id, u.imageUrl, u.imageType) " +
+                        "FROM UserImagesModel u WHERE u.user.id = :userId AND u.imageType = :imageType")
+        List<UserImagesDTO> findUserImagesWithoutUserInfo(
+                        @Param("userId") UUID userId,
+                        @Param("imageType") ImageType imageType);
 }
