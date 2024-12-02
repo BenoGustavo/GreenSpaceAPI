e30d2b67e6121778fed374a3503d7a1d16f7521e
Gustavo <gustavoleandrogorges@gmail.com>
2024-12-02 18:40:10 -0300

    feat: simplify upload endpoint by replacing RequestMapping with PostMapping in UserImagesController

--- src/main/java/com/greenspace/api/features/imagesManager/UserImagesController.java
+++ src/main/java/com/greenspace/api/features/imagesManager/UserImagesController.java
@@ -7,8 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -27,7 +27,7 @@ public class UserImagesController {
         this.userImagesService = userImagesService;
     }
 
-    @RequestMapping(value = "/upload", method = RequestMethod.POST, consumes = "multipart/form-data")
+    @PostMapping(value = "/upload", consumes = "multipart/form-data")
     public ResponseEntity<Response<Object>> uploadNewPicture(
             @RequestParam("file") MultipartFile file,
             @RequestParam("pictureName") String pictureName) {
