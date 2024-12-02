225893d4d755eaeabfca9d8130e11e0ad6f5a64a
Gustavo <gustavoleandrogorges@gmail.com>
2024-12-02 18:38:46 -0300

    feat: enable ENUM type for imageType in UserImagesModel for better database representation

--- src/main/java/com/greenspace/api/models/UserImagesModel.java
+++ src/main/java/com/greenspace/api/models/UserImagesModel.java
@@ -10,6 +10,8 @@ import com.greenspace.api.enums.ImageType;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -40,7 +42,7 @@ public class UserImagesModel {
     @Column(nullable = false, updatable = false)
     private String imageName;
 
-    // @Enumerated(EnumType.STRING)
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false, updatable = false)
     private ImageType imageType;
 
